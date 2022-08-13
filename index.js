ca6b2ef17c5e0c3eb12463f94ff2bf30d7cf6ffb
bilol <bilol09876@gmail.com>
2022-08-13 17:45:50 +0500

    one comit

---
+++ index.js
@@ -0,0 +1,46 @@
+let ism = "Biloliddin ";
+
+console.log(ism);
+
+let yosh = 19;
+
+console.log(yosh);
+
+let Cheksizlik = 996366014000000000n;
+
+console.log(Cheksizlik);
+
+let sizuylanganmsiz = false;
+
+console.log(sizuylanganmsiz);
+
+let birbalo = undefined;
+
+console.log(birbalo);
+
+let paspitringizbormi = null;
+
+console.log(paspitringizbormi);
+
+let yershardanmisz = Symbol("ha albata ");
+
+console.log(yershardanmisz);
+
+let qism = {
+  salom: " Hello world ",
+};
+
+alert("Salom bololar ");
+
+// sinf ishi tugad //
+
+// Uy ishi //
+
+let name1 = prompt("Ismingiz Kirting ");
+let sorname = prompt("Familyangizni kirting ");
+let tel = prompt("Tel nomerizi kirting  ");
+let age = prompt(" Yoshingizn kirting ");
+
+console.log(
+  `Ismi : ${name1}\nFamilyasi: ${sorname}\nTelefonraqam${tel}\nYoshi ${age}`
+);
